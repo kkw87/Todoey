a2fdbdfe954004e2759cd2350802e60043fbad23
Kevin Wang <kevinwang@kevins-macbook-pro-2.local>
2018-10-16 22:13:54 -0400

    Added table view data source/delegates, changed info.plist directory

---
+++ Todoey/View Controllers/ToDoListTableViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ToDoListTableViewController.swift
+//  Todoey
+//
+//  Created by Kevin Wang on 10/16/18.
+//  Copyright © 2018 Kevin Wang. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListTableViewController: UITableViewController {
+    
+    struct Storyboard {
+        static let CellReuseID = "ToDoItemCell"
+    }
+    
+    let itemArray = ["a", "b", "c"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return itemArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.CellReuseID, for: indexPath)
+
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+ 
+    //MARK: - Table view delegates
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let currentCell = tableView.cellForRow(at: indexPath)
+
+        if currentCell?.accessoryType == .checkmark {
+            currentCell?.accessoryType = .none
+        } else {
+            currentCell?.accessoryType = .checkmark
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)        
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
